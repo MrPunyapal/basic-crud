3a52dcb1772c0de53fcab2f06c6617cb0696cd01
Hosmel Quintana <hosmelq@gmail.com>
2023-12-06 12:12:26 -0600

    run pint

--- tests/Unit/SettingsTest.php
+++ tests/Unit/SettingsTest.php
@@ -4,15 +4,14 @@ namespace Tests\Unit;
 
 use App\Support\Settings;
 
-test('has categories', function() {
+test('has categories', function () {
     $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js', 'Java', 'C#'];
 
     $this->assertSame($categories, Settings::getCategories());
 });
 
-test('has tags', function() {
+test('has tags', function () {
     $tags = ['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'];
 
     $this->assertSame($tags, Settings::getTags());
 });
-

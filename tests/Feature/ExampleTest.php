104b47205c2b28e6432e26ce4110383f6a501e36
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-06 21:15:40 +0530

    Fix redirect in ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,5 @@
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(302);
+    $response->assertRedirect(route('posts.index'));
 });

55f0b6b9d74474c0816abaf894ef48a0acdac024
David Carr <dave.carr@vivedia.net>
2023-12-06 16:53:47 +0000

    converted test to pest

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -4,132 +4,119 @@ namespace Tests\Feature;
 
 use App\Models\Post;
 use Illuminate\Http\UploadedFile;
-use Tests\TestCase;
 
-class PostsTest extends TestCase
-{
-    public function test_root_redirects_to_posts(): void
-    {
-        $this
-            ->get('/')
-            ->assertRedirect(route('posts.index'));
-    }
+test('root redirects to posts', function() {
+    $this
+        ->get('/')
+        ->assertRedirect(route('posts.index'));
+});
 
-    public function test_can_see_posts(): void
-    {
-        $this
-            ->get(route('posts.index'))
-            ->assertOk()
-            ->assertViewIs('posts.index')
-            ->assertViewHas('posts')
-            ->assertViewHas('categories')
-            ->assertViewHas('tags');
-    }
+test('can see posts', function() {
+    $this
+        ->get(route('posts.index'))
+        ->assertOk()
+        ->assertViewIs('posts.index')
+        ->assertViewHas('posts')
+        ->assertViewHas('categories')
+        ->assertViewHas('tags');
+});
 
-    public function test_can_see_create_post_page(): void
-    {
-        $this
-            ->get(route('posts.create'))
-            ->assertOk()
-            ->assertViewIs('posts.create')
-            ->assertViewHas('categories')
-            ->assertViewHas('tags');
-    }
+test('can see create post page', function() {
+    $this
+        ->get(route('posts.create'))
+        ->assertOk()
+        ->assertViewIs('posts.create')
+        ->assertViewHas('categories')
+        ->assertViewHas('tags');
+});
 
-    public function test_can_create_post(): void
-    {
-        $image = UploadedFile::fake()->image('some-image.png');
+test('can create post', function() {
+    $image = UploadedFile::fake()->image('some-image.png');
 
-        $this
-            ->post(route('posts.store'), [
-                'title' => 'Test Title',
-                'slug' => 'test-title',
-                'content' => 'Test Content',
-                'image' => $image,
-                'category' => ['Laravel'],
-                'description' => 'this is the description',
-                'body' => 'this is the body',
-                'tags' => ['Eloquent'],
-            ])
-            ->assertSessionHasNoErrors()
-            ->assertRedirect(route('posts.index'));
+    $this
+        ->post(route('posts.store'), [
+            'title' => 'Test Title',
+            'slug' => 'test-title',
+            'content' => 'Test Content',
+            'image' => $image,
+            'category' => ['Laravel'],
+            'description' => 'this is the description',
+            'body' => 'this is the body',
+            'tags' => ['Eloquent'],
+        ])
+        ->assertSessionHasNoErrors()
+        ->assertRedirect(route('posts.index'));
 
-        $this->assertDatabaseHas('posts', [
-            'title' => 'Test Title'
-        ]);
-    }
+    $this->assertDatabaseHas('posts', [
+        'title' => 'Test Title'
+    ]);
+});
 
-    public function test_cannot_create_post_with_invalid_data(): void
-    {
-        $this
-            ->post(route('posts.store'), [])
-            ->assertSessionHasErrors([
-                'title' => 'The title field is required.',
-                'slug',
-                'category',
-                'description',
-                'body'
-            ])
-            ->assertRedirect();
-    }
+test('cannot create post with invalid data', function() {
+    $this
+        ->post(route('posts.store'), [])
+        ->assertSessionHasErrors([
+            'title' => 'The title field is required.',
+            'slug',
+            'category',
+            'description',
+            'body'
+        ])
+        ->assertRedirect();
+});
 
-    public function test_can_see_post_page(): void
-    {
-        $post = Post::factory()->create();
+test('can see post page', function() {
+    $post = Post::factory()->create();
 
-        $this
-            ->get(route('posts.show', $post))
-            ->assertOk()
-            ->assertViewIs('posts.show')
-            ->assertViewHas('post')
-            ->assertViewHas('categories')
-            ->assertViewHas('tags');
-    }
+    $this
+        ->get(route('posts.show', $post))
+        ->assertOk()
+        ->assertViewIs('posts.show')
+        ->assertViewHas('post')
+        ->assertViewHas('categories')
+        ->assertViewHas('tags');
+});
 
-    public function test_can_see_edit_post_page(): void
-    {
-        $post = Post::factory()->create();
+test('can see edit post page', function() {
+    $post = Post::factory()->create();
 
-        $this
-            ->get(route('posts.edit', $post))
-            ->assertOk()
-            ->assertViewIs('posts.edit')
-            ->assertViewHas('post')
-            ->assertViewHas('categories')
-            ->assertViewHas('tags');
-    }
+    $this
+        ->get(route('posts.edit', $post))
+        ->assertOk()
+        ->assertViewIs('posts.edit')
+        ->assertViewHas('post')
+        ->assertViewHas('categories')
+        ->assertViewHas('tags');
+});
 
-    public function test_can_edit_post(): void
-    {
-        $post = Post::factory()->create();
+test('can edit post', function() {
+    $post = Post::factory()->create();
 
-        $image = UploadedFile::fake()->image('some-image.png');
+    $image = UploadedFile::fake()->image('some-image.png');
 
-        $this
-            ->patch(route('posts.update', $post), [
-                'title' => 'updated Title',
-                'slug' => 'test-title',
-                'content' => 'Test Content',
-                'image' => $image,
-                'category' => ['Laravel'],
-                'description' => 'this is the description',
-                'body' => 'this is the body',
-                'tags' => ['Eloquent'],
-            ])
-            ->assertSessionHasNoErrors()
-            ->assertRedirect(route('posts.index'));
+    $this
+        ->patch(route('posts.update', $post), [
+            'title' => 'updated Title',
+            'slug' => 'test-title',
+            'content' => 'Test Content',
+            'image' => $image,
+            'category' => ['Laravel'],
+            'description' => 'this is the description',
+            'body' => 'this is the body',
+            'tags' => ['Eloquent'],
+        ])
+        ->assertSessionHasNoErrors()
+        ->assertRedirect(route('posts.index'));
 
-        $this->assertDatabaseHas('posts', [
-            'title' => 'updated Title'
-        ]);
-    }
+    $this->assertDatabaseHas('posts', [
+        'title' => 'updated Title'
+    ]);
+});
 
-    public function test_can_delete_post(): void
-    {
-        $post = Post::factory()->create();
+test('can delete post', function() {
+    $post = Post::factory()->create();
 
-        $this
-            ->delete(route('posts.destroy', $post))
-            ->assertRedirect(route('posts.index'));
-    }
-}
+    $this
+        ->delete(route('posts.destroy', $post))
+        ->assertRedirect(route('posts.index'));
+});
--- tests/Unit/SettingsTest.php
+++ tests/Unit/SettingsTest.php
@@ -3,21 +3,16 @@
 namespace Tests\Unit;
 
 use App\Support\Settings;
-use PHPUnit\Framework\TestCase;
 
-class SettingsTest extends TestCase
-{
-    public function test_has_categories(): void
-    {
-        $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js', 'Java', 'C#'];
+test('has_categories', function() {
+    $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js', 'Java', 'C#'];
 
-        $this->assertSame($categories, Settings::getCategories());
-    }
+    $this->assertSame($categories, Settings::getCategories());
+});
 
-    public function test_has_tags(): void
-    {
-        $tags = ['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'];
+test('has_tags', function() {
+    $tags = ['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'];
 
-        $this->assertSame($tags, Settings::getTags());
-    }
-}
+    $this->assertSame($tags, Settings::getTags());
+});
+

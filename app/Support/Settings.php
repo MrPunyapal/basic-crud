972ab2286ede5640488772aafee188b045b049cb
David Carr <dave.carr@vivedia.net>
2023-12-06 15:57:11 +0000

    added types

--- app/Support/Settings.php
+++ app/Support/Settings.php
@@ -4,16 +4,16 @@ namespace App\Support;
 
 class Settings
 {
-    private static $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js', 'Java', 'C#'];
+    private static array $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js', 'Java', 'C#'];
 
-    private static $tags = ['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'];
+    private static array $tags = ['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'];
 
-    public static function getCategories()
+    public static function getCategories(): array
     {
         return self::$categories;
     }
 
-    public static function getTags()
+    public static function getTags(): array
     {
         return self::$tags;
     }

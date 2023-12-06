cf10a60f49ece04bbf093b0b2c3f81e644b4d831
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-06 19:15:06 +0530

    Fix formatting in Settings.php and create_posts_table.php

--- app/Support/Settings.php
+++ app/Support/Settings.php
@@ -2,16 +2,19 @@
 
 namespace App\Support;
 
-class Settings {
-    private static $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js','Java','C#'];
+class Settings
+{
+    private static $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js', 'Java', 'C#'];
 
     private static $tags = ['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'];
 
-    public static function getCategories() {
+    public static function getCategories()
+    {
         return self::$categories;
     }
 
-    public static function getTags() {
+    public static function getTags()
+    {
         return self::$tags;
     }
 }
--- database/migrations/2023_12_05_085426_create_posts_table.php
+++ database/migrations/2023_12_05_085426_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
-            $table->string('slug',120)->unique();
+            $table->string('slug', 120)->unique();
             $table->string('description');
             $table->string('image')->nullable();
             $table->text('body');

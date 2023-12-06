aa7189048329891e464bccdec3868d2cfe8bc124
David Carr <dave.carr@vivedia.net>
2023-12-06 16:40:48 +0000

    added tests for posts

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Post;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class PostsTest extends TestCase
@@ -19,6 +19,117 @@ class PostsTest extends TestCase
     {
         $this
             ->get(route('posts.index'))
-            ->assertOk();
+            ->assertOk()
+            ->assertViewIs('posts.index')
+            ->assertViewHas('posts')
+            ->assertViewHas('categories')
+            ->assertViewHas('tags');
+    }
+
+    public function test_can_see_create_post_page(): void
+    {
+        $this
+            ->get(route('posts.create'))
+            ->assertOk()
+            ->assertViewIs('posts.create')
+            ->assertViewHas('categories')
+            ->assertViewHas('tags');
+    }
+
+    public function test_can_create_post(): void
+    {
+        $image = UploadedFile::fake()->image('some-image.png');
+
+        $this
+            ->post(route('posts.store'), [
+                'title' => 'Test Title',
+                'slug' => 'test-title',
+                'content' => 'Test Content',
+                'image' => $image,
+                'category' => ['Laravel'],
+                'description' => 'this is the description',
+                'body' => 'this is the body',
+                'tags' => ['Eloquent'],
+            ])
+            ->assertSessionHasNoErrors()
+            ->assertRedirect(route('posts.index'));
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'Test Title'
+        ]);
+    }
+
+    public function test_cannot_create_post_with_invalid_data(): void
+    {
+        $this
+            ->post(route('posts.store'), [])
+            ->assertSessionHasErrors([
+                'title' => 'The title field is required.',
+                'slug',
+                'category',
+                'description',
+                'body'
+            ])
+            ->assertRedirect();
+    }
+
+    public function test_can_see_post_page(): void
+    {
+        $post = Post::factory()->create();
+
+        $this
+            ->get(route('posts.show', $post))
+            ->assertOk()
+            ->assertViewIs('posts.show')
+            ->assertViewHas('post')
+            ->assertViewHas('categories')
+            ->assertViewHas('tags');
+    }
+
+    public function test_can_see_edit_post_page(): void
+    {
+        $post = Post::factory()->create();
+
+        $this
+            ->get(route('posts.edit', $post))
+            ->assertOk()
+            ->assertViewIs('posts.edit')
+            ->assertViewHas('post')
+            ->assertViewHas('categories')
+            ->assertViewHas('tags');
+    }
+
+    public function test_can_edit_post(): void
+    {
+        $post = Post::factory()->create();
+
+        $image = UploadedFile::fake()->image('some-image.png');
+
+        $this
+            ->patch(route('posts.update', $post), [
+                'title' => 'updated Title',
+                'slug' => 'test-title',
+                'content' => 'Test Content',
+                'image' => $image,
+                'category' => ['Laravel'],
+                'description' => 'this is the description',
+                'body' => 'this is the body',
+                'tags' => ['Eloquent'],
+            ])
+            ->assertSessionHasNoErrors()
+            ->assertRedirect(route('posts.index'));
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'updated Title'
+        ]);
+    }
+
+    public function test_can_delete_post(): void
+    {
+        $post = Post::factory()->create();
+
+        $this
+            ->delete(route('posts.destroy', $post))
+            ->assertRedirect(route('posts.index'));
     }
 }

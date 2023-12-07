5f9012ea716e5219ce60833859c5a726b88d9a64
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-07 19:39:43 +0530

    Update post show view with additional tags and featured badge

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('title', $post->title)
 @section('content')
-    <div class="row">
-        <div class="card">
+    <div class="row g-0 vh-100 p-0 m-0">
+        <div class="card col-8 m-auto">
             <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}" height="400">
             <div class="card-body">
                 <h5 class="card-title">
@@ -21,6 +21,20 @@
                 <p class="card-text trix-content">
                     {!! $post->body !!}
                 </p>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-between">
+                    <div>
+                        @foreach ($post->tags as $tag)
+                            <span class="badge bg-secondary">{{ $tag }}</span>
+                        @endforeach
+                    </div>
+                    <div>
+                        @if ($post->is_featured)
+                            <span class="badge bg-success">Featured</span>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
     </div>

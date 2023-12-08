a094c826a7900953007ec0d5b2bcdbee8873abed
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-08 23:12:18 +0530

    Add test for suffixed requests in ArchTest.php

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -8,6 +8,10 @@ test('controllers are suffixed with Controller')
     ->expect('App\Http\Controllers')
     ->toHaveSuffix('Controller');
 
+test('Requests are suffixed with Request')
+    ->expect('App\Http\Requests')
+    ->toHaveSuffix('Request');
+
 test('models not to be suffixed with Model')
     ->expect('App\Models')
     ->not->toHaveSuffix('Model');

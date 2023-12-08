4b8201c29c87ec3ad40a35bd6e9f8d9ea5a47bee
Rahul Jat <imrjat@gmail.com>
2023-12-08 17:06:15 +0530

    Add PEST test case for PostController search functionality

--- tests/Feature/Http/Controllers/PublishedPostsControllerTest.php
+++ tests/Feature/Http/Controllers/PublishedPostsControllerTest.php
@@ -17,5 +17,33 @@ test('can only see published posts', function () {
     get(route('posts.published'))
         ->assertOk()
         ->assertViewIs('posts.index')
-        ->assertViewHas('posts', fn ($posts) => $posts->where('published_at', '>=', now())->count() === 0);
+        ->assertViewHas('posts', fn($posts) => $posts->where('published_at', '>=', now())->count() === 0);
+});
+
+test('can search posts by title', function () {
+    // Create test data
+    $posts = Post::factory(4)->create([
+        'published_at' => now()->subDay(),
+    ]);
+    $post = $posts->first();
+
+    $searchTerm = $post->title;
+    // Execute the search
+    $response = $this->get(route('posts.published', ['search' => $searchTerm]));
+    // Assertions
+    $response->assertOk();
+    $response->assertViewIs('posts.index');
+    $response->assertViewHasAll([
+        'categories',
+        'posts',
+    ]);
+
+    // Check if the matching post is present in the view
+    $response->assertSeeText($searchTerm);
+
+    // Check if non-matching posts are not present in the view
+
+    foreach ($posts->skip(1) as $post) {
+        $response->assertDontSeeText($post->title);
+    }
 });

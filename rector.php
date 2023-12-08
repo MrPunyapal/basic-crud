dd83fc8f1aebbb6d40484f9d4b1201e9cfd8fcbf
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-08 23:23:46 +0530

    Add driftingly/rector-laravel and rector/rector to composer.json

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/app',
+        // __DIR__.'/bootstrap',
+        // __DIR__.'/config',
+        // __DIR__.'/public',
+        // __DIR__.'/resources',
+        // __DIR__.'/routes',
+        // __DIR__.'/tests',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LaravelSetList::LARAVEL_100,
+    ]);
+};

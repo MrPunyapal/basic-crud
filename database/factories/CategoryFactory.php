c9b8a6afc7aebc0c374824b1d0c9f40e3274be55
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-12 20:00:00 +0530

    Update CategoryFactory to generate unique titles

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->words(2, true),
+            'title' => $this->faker->unique()->words(2, true),
         ];
     }
 }

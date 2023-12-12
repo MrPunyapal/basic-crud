945f73bbe49e1ea1a05ebfa78c6d54eeccda47c6
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-12 19:20:58 +0530

    Add Category model with fillable fields and relationship to posts

---
+++ app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}

1951529d3caa5a654c97772d17bc43adeeae5f4d
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-12 19:21:04 +0530

    Rename 'category' column to 'category_id' in posts table

---
+++ database/migrations/2023_12_12_134928_change_category_to_category_id_in_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->renameColumn('category', 'category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->renameColumn('category_id', 'category');
+            $table->dropForeign(['category_id']);
+        });
+    }
+};

dbf89c159dd8dad5cd7bbd6f9b3310ffc02c07e5
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-12 19:25:56 +0530

    Remove test for categories in SettingsTest.php

--- tests/Unit/SettingsTest.php
+++ tests/Unit/SettingsTest.php
@@ -4,12 +4,6 @@ namespace Tests\Unit;
 
 use App\Support\Settings;
 
-test('has categories', function () {
-    $categories = ['Laravel', 'PHP', 'JavaScript', 'Vue.js', 'React.js', 'Angular.js', 'Java', 'C#'];
-
-    $this->assertSame($categories, Settings::getCategories());
-});
-
 test('has tags', function () {
     $tags = ['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'];
 

763c16a215f74338ab78f702eb2eaa2678c93629
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-16 19:18:16 +0530

    Add SetLocaleControllerTest.php for testing locale setting

---
+++ tests/Feature/Http/Controllers/SetLocaleControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use function Pest\Laravel\from;
+
+test('can set locale', function ($locale) {
+    from(route('posts.index'))
+        ->get(route('set-locale', $locale))
+        ->assertStatus(302)
+        ->assertRedirect(route('posts.index'))
+        ->assertCookie('locale', $locale);
+})->with(['en', 'fr']);

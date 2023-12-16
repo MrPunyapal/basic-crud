c4df1d33946d2176ffaeb55e634d6019823558c6
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-16 16:50:28 +0530

    Refactor FeaturedStatusTest.php

--- tests/Unit/FeaturedStatusTest.php
+++ tests/Unit/FeaturedStatusTest.php
@@ -6,26 +6,54 @@ it('returns the correct label for NOT_FEATURED', function () {
     $status = FeaturedStatus::NOT_FEATURED;
     $label = $status->label();
 
-    expect($label)->toBe('No');
+    expect($label)->toBe('Not Featured');
 });
 
 it('returns the correct label for FEATURED', function () {
     $status = FeaturedStatus::FEATURED;
     $label = $status->label();
 
-    expect($label)->toBe('Yes');
+    expect($label)->toBe('Featured');
 });
 
-// it('returns the correct bg_color for NOT_FEATURED', function () {
-//     $status = FeaturedStatus::NOT_FEATURED;
-//     $bgColor = $status->bg_color();
+it('returns the correct color for NOT_FEATURED', function () {
+    $status = FeaturedStatus::NOT_FEATURED;
+    $color = $status->color();
 
-//     expect($bgColor)->toBe('bg-info');
-// });
+    expect($color)->toBe('bg-info');
+});
 
-// it('returns the correct bg_color for FEATURED', function () {
-//     $status = FeaturedStatus::FEATURED;
-//     $bgColor = $status->bg_color();
+it('returns the correct color for FEATURED', function () {
+    $status = FeaturedStatus::FEATURED;
+    $color = $status->color();
 
-//     expect($bgColor)->toBe('bg-success');
-// });
+    expect($color)->toBe('bg-success');
+});
+
+it('returns the correct change button label for NOT_FEATURED', function () {
+    $status = FeaturedStatus::NOT_FEATURED;
+    $btnLabel = $status->changeBtnLabel();
+
+    expect($btnLabel)->toBe('Feature');
+});
+
+it('returns the correct change button label for FEATURED', function () {
+    $status = FeaturedStatus::FEATURED;
+    $btnLabel = $status->changeBtnLabel();
+
+    expect($btnLabel)->toBe('Unfeature');
+});
+
+it('returns the correct change button color for NOT_FEATURED', function () {
+    $status = FeaturedStatus::NOT_FEATURED;
+    $btnColor = $status->changeBtnColor();
+
+    expect($btnColor)->toBe('btn-warning');
+});
+
+it('returns the correct change button color for FEATURED', function () {
+    $status = FeaturedStatus::FEATURED;
+    $btnColor = $status->changeBtnColor();
+
+    expect($btnColor)->toBe('btn-secondary');
+});

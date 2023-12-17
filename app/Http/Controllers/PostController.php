2b03aeb00d86ec6f943cfe9ba4d1d2edd30973ca
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-17 21:16:17 +0530

    Refactor pagination in PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,8 +29,7 @@ class PostController extends Controller
                     fn ($query) => $query->sortBy($request->input('sortBy'), $request->input('direction')),
                     fn ($query) => $query->latest(),
                 )
-                ->paginate(10)
-                ->withQueryString(),
+                ->paginate(10),
         ]);
     }
 

5772580952abdfda55e633a0a42edb576f9138d5
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-17 14:14:57 +0530

    Add unit tests for QueryResolver class

--- tests/Unit/QueryResolverTest.php
+++ tests/Unit/QueryResolverTest.php
@@ -35,3 +35,107 @@ it('can resolve sort query', function ($query, $expectedQuery) {
         ['published' => true, 'search' => 'test', 'sortBy' => 'title', 'direction' => 'asc'],
     ],
 ]);
+
+it('can resolve sort arrow', function ($query, $expectedArrow) {
+
+    get(route('posts.index', $query));
+
+    $resolver = new QueryResolver();
+
+    $sortArrow = $resolver->sortArrow('title');
+
+    expect($sortArrow)->toEqual($expectedArrow);
+})->with([
+    [
+        ['sortBy' => 'title', 'direction' => 'asc', 'page' => 1],
+        '&darr;',
+    ],
+    [
+        ['sortBy' => 'title', 'direction' => 'desc'],
+        '&uarr;',
+    ],
+    [
+        ['published' => true, 'search' => 'test', 'page' => 1],
+        '&darr;&uarr;',
+    ],
+]);
+
+it('can resolve published query', function ($query, $expectedQuery) {
+
+    get(route('posts.index', $query));
+
+    $resolver = new QueryResolver();
+
+    $publishedQuery = $resolver->publishedQuery();
+
+    expect($publishedQuery)->toEqual($expectedQuery);
+})->with([
+    [
+        ['published' => true, 'search' => 'test', 'page' => 1],
+        ['search' => 'test', 'published' => null],
+    ],
+    [
+        ['search' => 'test', 'page' => 1],
+        ['published' => true, 'search' => 'test'],
+    ],
+]);
+
+it('can resolve published label', function ($query, $expectedLabel) {
+
+    get(route('posts.index', $query));
+
+    $resolver = new QueryResolver();
+
+    $publishedLabel = $resolver->publishedLabel();
+
+    expect($publishedLabel)->toEqual($expectedLabel);
+})->with([
+    [
+        ['published' => true, 'search' => 'test', 'page' => 1],
+        'All Posts',
+    ],
+    [
+        ['search' => 'test', 'page' => 1],
+        'Published Posts',
+    ],
+]);
+
+it('can resolve search query', function ($query, $expectedQuery) {
+
+    get(route('posts.index', $query));
+
+    $resolver = new QueryResolver();
+
+    $searchQuery = $resolver->searchQuery();
+
+    expect($searchQuery)->toEqual($expectedQuery);
+})->with([
+    [
+        ['published' => true, 'search' => 'test', 'page' => 1],
+        ['published' => true],
+    ],
+    [
+        ['published' => true, 'page' => 1, 'sortBy' => 'title', 'direction' => 'asc'],
+        ['published' => true, 'sortBy' => 'title', 'direction' => 'asc'],
+    ],
+]);
+
+it('can resolve search value', function ($query, $expectedValue) {
+
+    get(route('posts.index', $query));
+
+    $resolver = new QueryResolver();
+
+    $searchValue = $resolver->searchValue();
+
+    expect($searchValue)->toEqual($expectedValue);
+})->with([
+    [
+        ['published' => true, 'search' => 'test', 'page' => 1],
+        'test',
+    ],
+    [
+        ['published' => true, 'page' => 1, 'sortBy' => 'title', 'direction' => 'asc'],
+        null,
+    ],
+]);

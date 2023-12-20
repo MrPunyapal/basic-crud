e4c8496415b5b6076789c8c6daab7208f6d88dd1
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-20 20:44:31 +0530

    Add columns() method to PostSortColumnsEnum

--- app/Enums/PostSortColumnsEnum.php
+++ app/Enums/PostSortColumnsEnum.php
@@ -7,6 +7,9 @@ enum PostSortColumnsEnum: string
     case TITLE = 'title';
     case IsFeatured = 'is_featured';
 
+    /**
+     * @return  array<int, string>
+     */
     public static function columns(): array
     {
         return array_column(self::cases(), 'value');

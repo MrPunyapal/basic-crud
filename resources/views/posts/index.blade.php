c32a57038d9ebdc575597f17681b3fca50b709ac
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-26 20:48:20 +0530

    Update index.blade.php layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,7 @@
         @session('success')
             <div class="bg-green-200 text-green-800 p-4 my-4">
                 {{ $value }}
+                <span class="float-right cursor-pointer text-xl" onclick="this.parentElement.remove()">x</span>
             </div>
         @endsession
 
@@ -40,7 +41,7 @@
                     <input type="hidden" name="{{ $key }}" value="{{ $value }}">
                 @endforeach
                 <div class="flex items-center">
-                    <input type="text" name="search" class="border border-gray-300 rounded-l-md py-2 px-4"
+                    <input type="search" name="search" class="border border-gray-300 rounded-l-md py-2 px-4"
                         placeholder="{{ __('posts.form.Search here') }}" value="{{ $queryResolver->searchValue() }}">
                     <button type="submit"
                         class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r-md">
@@ -49,7 +50,7 @@
                 </div>
             </form>
         </div>
-        <table class="table-auto border-collapse border-spacing-2 border border-slate-500">
+        <table class="table-auto border border-slate-500">
             <thead>
                 <tr>
                     <th class="border border-slate-600">#</th>
@@ -76,31 +77,31 @@
             <tbody>
                 @forelse ($posts as $post)
                     <tr>
-                        <td class="border border-slate-600">
+                        <td class="border border-slate-600 py-2 px-4">
                             {{ $loop->index + $posts->firstItem() }}
                         </td>
-                        <td class="border border-slate-600">
+                        <td class="border border-slate-600 py-2 px-4">
                             <a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post->title }}</a>
                         </td>
-                        <td class="border border-slate-600">{{ $post->category_title }}</td>
-                        <td class="border border-slate-600">{{ $post->is_featured->BooleanLabel() }}</td>
-                        <td class="border border-slate-600">{{ $post->created_at->format('d/m/Y H:i:s') }}</td>
-                        <td class="border border-slate-600">{{ $post->updated_at->since() }}</td>
-                        <td class="border border-slate-600">
+                        <td class="border border-slate-600 py-2 px-4">{{ $post->category_title }}</td>
+                        <td class="border border-slate-600 py-2 px-4">{{ $post->is_featured->BooleanLabel() }}</td>
+                        <td class="border border-slate-600 py-2 px-4">{{ $post->created_at->format('d/m/Y H:i:s') }}</td>
+                        <td class="border border-slate-600 py-2 px-4">{{ $post->updated_at->since() }}</td>
+                        <td class="border border-slate-600 py-2 px-4">
                             <form action="{{ route('posts.featured', ['post' => $post]) }}" method="POST" class="inline">
                                 @csrf
                                 @method('PATCH')
                                 <input type="submit" value="{{ $post->is_featured->changeBtnLabel() }}"
-                                    class="{{ $post->is_featured->changeBtnColor() }} text-white font-bold py-2 px-4 rounded">
+                                    class="{{ $post->is_featured->changeBtnColor() }} text-white font-bold py-1 px-2 rounded w-24">
                             </form>
                             <a href="{{ route('posts.edit', ['post' => $post]) }}"
-                                class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">{{ __('posts.show.Edit') }}</a>
+                                class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-2 rounded">{{ __('posts.show.Edit') }}</a>
                             <form action="{{ route('posts.destroy', ['post' => $post]) }}" method="POST" class="inline"
                                 onsubmit="return confirm('{{ __('posts.form.Are you sure you want to delete this post?') }}')">
                                 @csrf
                                 @method('DELETE')
                                 <input type="submit" value="{{ __('posts.show.Delete') }}"
-                                    class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+                                    class="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded">
                             </form>
                         </td>
                     </tr>

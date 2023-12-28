7161313198bb3318e1655f5524465c859e501b26
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-28 21:55:20 +0530

    Add API resource routes for posts

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('posts', 'Api\PostController');
+
+// similar to:
+// Route::get('/posts', [PostController::class, 'index']);
+// Route::post('/posts', [PostController::class, 'store']);
+// Route::get('/posts/{post}', [PostController::class, 'show']);
+// Route::put('/posts/{post}', [PostController::class, 'update']);
+// Route::delete('/posts/{post}', [PostController::class, 'destroy']);

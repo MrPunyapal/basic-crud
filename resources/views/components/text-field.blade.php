ac32cb3af5362c6cb4ed820996613e7c12da69f5
Hosmel Quintana <hosmelq@gmail.com>
2023-12-31 11:57:36 -0600

    remove @tailwindcss/forms

--- resources/views/components/text-field.blade.php
+++ resources/views/components/text-field.blade.php
@@ -4,7 +4,13 @@
 ])
 
 @php
-    $class = 'block w-full rounded-lg bg-white border text-sm/6 py-1.5 px-3 border-zinc-950/10';
+    $class = implode(' ', [
+        'appearance-none block w-full rounded-lg bg-white border text-sm/6 py-[calc(theme(spacing[1.5])+theme(spacing.px))] px-3 border-zinc-950/10',
+        // focus
+        'focus:border-zinc-950/20 focus:outline focus:outline-offset-2 focus:outline-2 focus:outline-blue-500',
+        // hover
+        'hover:border-zinc-950/20',
+    ]);
 
     $attrs = $attributes->merge([
         'class' => $class,

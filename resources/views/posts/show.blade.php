2c9f2ca9576d145a9dc9815c930f859e8fc87346
Hosmel Quintana <hosmelq@gmail.com>
2023-12-31 13:36:42 -0600

    tweaks

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,39 +2,48 @@
 @extends('layouts.app')
 @section('title', $post->title)
 @section('content')
-    <div class="flex flex-col p-0 m-0">
-        <div class="flex justify-end mt-2">
-            <a href="{{ route('posts.index') }}" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">{{ __('posts.show.View All') }}</a>
-        </div>
-        <div class="bg-white shadow-lg rounded-lg overflow-hidden mx-auto">
-            <img src="{{ $post->image }}" class="w-full" alt="{{ $post->title }}" height="400">
-            <div class="p-6">
-                <h5 class="text-xl font-bold mb-2">
-                    {{ $post->title }}
-                    <form action="{{ route('posts.featured', ['post' => $post]) }}" method="POST" class="inline">
-                        @csrf
-                        @method('PATCH')
-                        <input type="submit" value="{{ $post->is_featured->changeBtnLabel() }}"
-                            class="{{ $post->is_featured->changeBtnColor() }} text-white font-bold py-2 px-4 rounded">
-                    </form>
-                    <a href="{{ route('posts.edit', ['post' => $post]) }}"
-                        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">{{ __('posts.show.Edit') }}</a>
-                    <form action="{{ route('posts.destroy', ['post' => $post]) }}" method="POST" class="inline"
-                        onsubmit="return confirm('{{ __('posts.form.Are you sure you want to delete this post?') }}')">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="{{ __('posts.show.Delete') }}"
-                            class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-                    </form>
-                </h5>
-                <h6 class="text-sm text-gray-500 mb-2">
-                    {{ $post->category_title }} {{ optional($post->published_at)->format('M d, Y h:i a') }}
-                </h6>
-                <p class="text-gray-700 trix-content">
+    <div class="max-w-xl w-full mx-auto flex flex-col p-0 m-0">
+        <x-button class="self-end" :href="route('posts.index')">
+            {{ __('posts.show.View All') }}
+        </x-button>
+        <div class="bg-gray-50 mt-4 shadow overflow-hidden rounded-lg">
+            <img src="{{ $post->image }}" class="w-full" alt="{{ $post->title }}" height="400" />
+            <div class="px-3 py-4 bg-white shadow-sm rounded m-1">
+                <div class="flex gap-x-4">
+                    <div class="flex-1">
+                        <h1 class="text-2xl font-bold mb-2">
+                            {{ $post->title }}
+                        </h1>
+                        <time class="text-sm text-zinc-500 mb-2" datetime="{{ $post->published_at }}">
+                            {{ $post->category_title }} {{ optional($post->published_at)->format('M d, Y h:i a') }}
+                        </time>
+                    </div>
+                    <div class="flex self-start gap-x-4">
+                        <form action="{{ route('posts.featured', ['post' => $post]) }}" method="POST" class="inline">
+                            @csrf
+                            @method('PATCH')
+                            <x-button :color="$post->is_featured->buttonColor()" type="submit">
+                                {{ $post->is_featured->changeBtnLabel() }}
+                            </x-button>
+                        </form>
+                        <x-button color="blue" :href="route('posts.edit', ['post' => $post])">
+                            {{ __('posts.show.Edit') }}
+                        </x-button>
+                        <form action="{{ route('posts.destroy', ['post' => $post]) }}" method="POST" class="inline"
+                              onsubmit="return confirm('{{ __('posts.form.Are you sure you want to delete this post?') }}')">
+                            @csrf
+                            @method('DELETE')
+                            <x-button color="red" type="submit">
+                                {{ __('posts.show.Delete') }}
+                            </x-button>
+                        </form>
+                    </div>
+                </div>
+                <p class="text-gray-700 mt-4 trix-content">
                     {!! $post->content !!}
                 </p>
             </div>
-            <div class="bg-gray-100 p-6">
+            <div class="bg-gray-50 px-3 py-4">
                 <div class="flex justify-between">
                     <div>
                         @foreach ($post->tags as $tag)

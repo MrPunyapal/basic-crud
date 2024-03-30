8351c32dd00b63d1eae50efc80a09f683d63ac68
Punyapal Shah <mrpunyapal@gmail.com>
2024-03-30 17:53:41 +0530

    fix the locale issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(append: [
+            \App\Http\Middleware\SetLocale::class,
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

378d27d27a0f90eaa39a4ca75b7df5dd8b9f311d
Punyapal Shah <mrpunyapal@gmail.com>
2024-08-07 20:33:52 +0530

    chore: Refactor Category model and add missing property annotations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,13 +4,28 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Database\Factories\CategoryFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Post> $posts
+ * @property-read int|null $posts_count
+ *
+ * @mixin \Eloquent
+ */
 class Category extends Model
 {
+    /** @use HasFactory<CategoryFactory> */
     use HasFactory, SoftDeletes;
 
     protected $fillable = [

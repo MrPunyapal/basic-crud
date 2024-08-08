943a62d6159a3b501f0080b0c91c2005e042110a
Punyapal Shah <mrpunyapal@gmail.com>
2024-08-08 21:06:37 +0530

    refactor: Update User model to use list<string> for mass assignable and hidden attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable // implements MustVerifyEmail
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -57,7 +57,7 @@ class User extends Authenticatable // implements MustVerifyEmail
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',

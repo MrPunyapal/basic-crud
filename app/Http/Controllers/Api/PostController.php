015410e535441d186d012861a3f4dec88855911e
Punyapal Shah <mrpunyapal@gmail.com>
2024-09-05 23:36:50 +0530

    Update PostController.php to return JsonResource instead of JsonResponse

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -8,53 +8,52 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(): JsonResource
     {
-        return response()->json(Post::all());
+        return JsonResource::collection(Post::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request): JsonResponse
+    public function store(StorePostRequest $request): JsonResource
     {
-        $post = Post::query()->create($request->validated());
-
-        return response()->json($post, 201);
+        return new JsonResource(Post::query()->create($request->validated()));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post): JsonResponse
+    public function show(Post $post): JsonResource
     {
-        return response()->json($post);
+        return new JsonResource($post);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post): JsonResponse
+    public function update(UpdatePostRequest $request, Post $post): JsonResource
     {
         $post->update($request->validated());
 
-        return response()->json($post);
+        return new JsonResource($post);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post): JsonResponse
+    public function destroy(Post $post): Response
     {
         $post->delete();
 
-        return response()->json(null, 204);
+        return response()->noContent();
     }
 }

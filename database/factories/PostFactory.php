2c2f524e779ca5e18b9f538a3828db4e710f9923
Punyapal Shah <mrpunyapal@gmail.com>
2025-01-01 19:44:35 +0530

    refactor PostFactory to use a custom fake image URL generator

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -27,12 +27,20 @@ class PostFactory extends Factory
             'title' => $this->faker->words(5, true),
             'slug' => $this->faker->slug,
             'description' => $this->faker->sentence,
-            'image' => $this->faker->imageUrl(),
+            'image' => $this->fakeImageUrl(),
             'content' => $this->faker->randomHtml(),
             'published_at' => $this->faker->dateTimeBetween('-1 month', '+3 months'),
             'category_id' => CategoryFactory::new(),
             'tags' => $this->faker->randomElements(['Eloquent', 'Blade', 'Migrations', 'Seeding', 'Routing', 'Controllers', 'Middleware', 'Requests', 'Responses', 'Views', 'Forms', 'Validation', 'Mail', 'Notifications'], $this->faker->numberBetween(1, 3)),
             'is_featured' => $this->faker->randomElement(array_column(FeaturedStatus::cases(), 'value')),
         ];
+    }
+
+    /**
+     * Generate a fake image URL.
+     */
+    private function fakeImageUrl(): string
+    {
+        return 'https://fakeimg.pl/350x200/?text='.$this->faker->word;
     }
 }

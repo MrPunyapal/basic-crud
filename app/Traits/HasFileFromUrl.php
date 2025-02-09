4b756b632a75f14810a4e7db3d7c9a846049c049
Punyapal Shah <mrpunyapal@gmail.com>
2025-02-09 14:28:00 +0530

    refactor HasFileFromUrl trait to utilize FileUploaderFromUrl for file resolution from URL

--- app/Traits/HasFileFromUrl.php
+++ app/Traits/HasFileFromUrl.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
+use App\Support\FileUploaderFromUrl;
 use Illuminate\Http\UploadedFile;
 
 trait HasFileFromUrl
@@ -11,11 +12,11 @@ trait HasFileFromUrl
     public function resolveFileFromUrl(string $field): void
     {
         if (! $this->hasFile($field) && filter_var($this->get($field), FILTER_VALIDATE_URL)) {
-            $file = UploadedFile::makeFromUrl(
+            $file = (new FileUploaderFromUrl)(
                 (string) $this->string($field)
             );
 
-            if ($file !== null) {
+            if ($file instanceof UploadedFile) {
                 $this->merge([
                     $field => $file,
                 ]);

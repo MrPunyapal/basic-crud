bbccc7bdbcf587cc968ee81bc4b2b96af52d955e
Punyapal Shah <mrpunyapal@gmail.com>
2025-02-09 14:28:03 +0530

    remove unused UploadedFile macro for URL handling in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 use Override;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,32 +18,5 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        // @codeCoverageIgnoreStart
-        UploadedFile::macro('makeFromUrl', function (string $url): ?UploadedFile {
-            $tempFile = tempnam(sys_get_temp_dir(), Str::random(32));
-
-            if ($tempFile === false) {
-                return null;
-            }
-
-            $file = file_get_contents($url);
-
-            if ($file === false) {
-                return null;
-            }
-
-            file_put_contents($tempFile, $file);
-
-            return new UploadedFile(
-                $tempFile,
-                basename($url),
-                mime_content_type($tempFile) ?: null,
-                null,
-                true
-            );
-        });
-        // @codeCoverageIgnoreEnd
-    }
+    public function boot(): void {}
 }

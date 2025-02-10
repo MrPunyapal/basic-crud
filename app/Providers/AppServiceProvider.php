9691e7a2e9d5a72765792805970663b29e1ccc72
Punyapal Shah <mrpunyapal@gmail.com>
2025-02-10 13:02:15 +0530

    add macro to UploadedFile for creating instances from a URL using FileUploaderFromUrl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Support\FileUploaderFromUrl;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\ServiceProvider;
 use Override;
 
@@ -18,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        UploadedFile::macro('makeFromUrl', fn (string $url): ?UploadedFile => (new FileUploaderFromUrl)($url));
+    }
 }

c207b063655a9e957008bb6aa87ebe93039d7e98
Punyapal Shah <mrpunyapal@gmail.com>
2025-02-23 11:41:08 +0530

    refactor: inject TogglePostFeatureAction into FeaturedPostController

--- app/Http/Controllers/FeaturedPostController.php
+++ app/Http/Controllers/FeaturedPostController.php
@@ -4,20 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Actions\Posts\TogglePostFeatureAction;
 use App\Enums\FeaturedStatus;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
 
 class FeaturedPostController extends Controller
 {
-    public function __invoke(Post $post): RedirectResponse
+    public function __invoke(Post $post, TogglePostFeatureAction $action): RedirectResponse
     {
         $isFeatured = $post->is_featured === FeaturedStatus::Featured;
-        $post->update([
-            'is_featured' => $isFeatured
-                ? FeaturedStatus::NotFeatured
-                : FeaturedStatus::Featured,
-        ]);
+        $action->execute($post);
 
         return back()->with('success', __('posts.messages.Post '.($isFeatured ? 'unfeatured' : 'featured').' successfully'));
     }

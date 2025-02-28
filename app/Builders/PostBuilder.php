fb10303c6f743ab6867dd1caa4f7128ceaf717bc
Punyapal Shah <mrpunyapal@gmail.com>
2025-02-28 16:30:54 +0530

    feat: add filter method to PostBuilder for searching, publishing, and sorting posts

--- app/Builders/PostBuilder.php
+++ app/Builders/PostBuilder.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Builders;
 
+use App\Enums\PostSortColumnsEnum;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Fluent;
 
 /**
  * @template TModelClass of \App\Models\Post
@@ -31,6 +33,30 @@ final class PostBuilder extends Builder
     public function published(): static
     {
         $this->where('published_at', '<=', now());
+
+        return $this;
+    }
+
+    /**
+     * @param  Fluent<string, mixed>  $filters
+     */
+    public function filter(Fluent $filters): static
+    {
+        $this->when($filters->string('search')->toString(), function (PostBuilder $query, string $search): void {
+            $query->search($search);
+        })
+            ->when($filters->get('published'), function (PostBuilder $query): void {
+                $query->published();
+            })
+            ->when(
+                in_array($filters->get('sortBy'), PostSortColumnsEnum::columns(), true),
+                function (PostBuilder $query) use ($filters): void {
+                    $query->sortBy($filters->string('sortBy')->toString(), $filters->string('direction')->toString());
+                },
+                function (PostBuilder $query): void {
+                    $query->latest();
+                },
+            );
 
         return $this;
     }

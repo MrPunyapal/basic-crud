bdf1bc921bf861ab9a1bbbcd19ee979bc8b29af6
Punyapal Shah <mrpunyapal@gmail.com>
2025-02-28 20:06:22 +0530

    feat: enhance AppServiceProvider with configuration methods for commands, models, dates, and password validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Support\FileUploaderFromUrl;
-use Illuminate\Http\UploadedFile;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 use Override;
 
 final class AppServiceProvider extends ServiceProvider
@@ -22,6 +25,44 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        UploadedFile::macro('makeFromUrl', fn (string $url): ?UploadedFile => (new FileUploaderFromUrl)($url));
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configurePasswordValidation();
+        $this->configureDates();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    /**
+     * Configure the dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the models.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::unguard();
+    }
+
+    /**
+     * Configure the password validation rules.
+     */
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(fn () => $this->app->isProduction() ? Password::min(8)->uncompromised() : null);
     }
 }

eff6458c0f822c9e27b20db836d4ae0b6d1b726c
Punyapal Shah <mrpunyapal@gmail.com>
2025-11-11 23:57:31 +0530

    refactor: add name to API route group for better organization

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->name('api.')->group(function () {
     Route::apiResource('posts', PostController::class);
     // similar to:
     // Route::get('/posts', [PostController::class, 'index']);
